8eae9535bb67cd134bde5b032b9fe1f0fb1e682d
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2025-02-10 18:17:44 +0100

    Feat: Fix header error

--- app/(client)/login/page.jsx
+++ app/(client)/login/page.jsx
@@ -2,7 +2,7 @@ import AuthComponent from "@/components/LoginComponent";
 
 const Login = () => {
   return (
-    <div className="bg-gradient-to-b from-gray-900 to-black">
+    <div className="bg-gradient-to-b from-gray-900 to-black main-content">
       <div className="py-20">
         <AuthComponent />
       </div>
